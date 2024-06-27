cce6c6109b9a2035d4c4043cb1008b10a9eec257
alexeykozlov <akozlov@bravadohealth.com>
2024-06-27 10:25:38 -0400

    next commit

---
+++ data/deliveryDetails.js
@@ -0,0 +1,8 @@
+export const deliveryDetails = {
+    firstName: "Sherlok",
+    lastName: "Holmes",
+    street: "Park de fontan",
+    postCode: "33577",
+    city: "Miami",
+    country: "United States of America"
+}
